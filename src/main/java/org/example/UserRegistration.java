8e2e7127a752e5aa617e92ca2e205b1c29dd45a1
Kuldeep147 <kuldeep.bargurjar@gmail.com>
2022-12-30 12:20:31 +0530

    Adding Junit test

---
+++ src/main/java/org/example/UserRegistration.java
@@ -0,0 +1,80 @@
+package org.example;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    String firstName;
+    String lastName;
+    String eMail;
+    String phoneNumber;
+    String password;
+    public static void main(String[] args) {
+        UserRegistration users = new UserRegistration();
+        users.checkEmail();
+
+    }
+    public String addUser(String[] arr){
+        String fName = arr[0];
+        Pattern p1 = Pattern.compile("^[A-Z]+[a-z]{2,}");
+        Matcher mat = p1.matcher(fName);
+        boolean b = mat.matches();
+        if(b){
+            firstName = fName;
+        }else{
+            return "Sad";
+        }
+        String lName = arr[1];
+        Pattern p2 = Pattern.compile("^[A-Z]+[a-z]{2,}");
+        Matcher l = p2.matcher(lName);
+        boolean b1 = l.matches();
+        if(b1){
+            lastName = lName;
+        }else{
+            return "Sad";
+        }
+        String email = arr[2];
+        Pattern p3 = Pattern.compile("[a-zA-Z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-za-z]{2,})$");
+        Matcher ema = p3.matcher(email);
+        boolean b2 = ema.matches();
+        if(b2){
+            eMail = email;
+        }else{
+            return "Sad";
+        }
+        String pNum = arr[3];
+        Pattern p4 = Pattern.compile("^(\\d{2}[- .]?){1}\\d{10}$");
+        Matcher ph = p4.matcher(pNum);
+        boolean b3 = ph.matches();
+        if(b3){
+            phoneNumber = pNum;
+        }else{
+            return "Sad";
+        }
+        String pass = arr[4];
+        Pattern p5 = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()+-=]).{8,}$");
+        Matcher pas = p5.matcher(pass);
+        boolean b4 = pas.matches();
+        if(b4){
+            password = pass;
+        }else{
+            return "Sad";
+        }
+        return "Happy";
+
+    }
+    public void checkEmail(){
+        System.out.println("Enter email id : ");
+        Scanner sb = new Scanner(System.in);
+        String email = sb.nextLine();
+        Pattern p3 = Pattern.compile("[a-zA-Z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-za-z]{2,})$");
+        Matcher ema = p3.matcher(email);
+        boolean b2 = ema.matches();
+        if(b2){
+            System.out.println("Valid email...");
+        }else{
+            System.out.println("Invalid email id...");
+        }
+    }
+}
---
+++ src/test/java/TestUserRegistration.java
@@ -0,0 +1,13 @@
+import org.example.UserRegistration;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class TestUserRegistration {
+    UserRegistration user = new UserRegistration();
+    @Test
+    public void testAddUser(){
+        String[] arr = {"Kuldeep","Badgujar","Kuldeep.badgujar@gmail.com","91 7878989800","HashTable@4567"};
+        assertEquals("Happy",user.addUser(arr));
+        String[] array = {"Kuldeep","Badgujar","Kuldeep.badgujar@gmail.com","91 787898980","HashTable@4567"};
+        assertEquals("Sad",user.addUser(array));
+    }
+}
