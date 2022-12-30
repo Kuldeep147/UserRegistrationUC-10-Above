2952d3f0d317ea2fae6ef05d81aefe2a3e13ad62
Kuldeep147 <kuldeep.bargurjar@gmail.com>
2022-12-30 12:24:43 +0530

    Adding test cases to validate user registration

--- src/test/java/TestUserRegistration.java
+++ src/test/java/TestUserRegistration.java
@@ -9,5 +9,7 @@ public class TestUserRegistration {
         assertEquals("Happy",user.addUser(arr));
         String[] array = {"Kuldeep","Badgujar","Kuldeep.badgujar@gmail.com","91 787898980","HashTable@4567"};
         assertEquals("Sad",user.addUser(array));
+        String[] array1 = {"Kuldeep","Badgujar","Kuldeep.badgujar@gmail.com","91 7878989809","HashTable@4567"};
+        assertEquals("Happy",user.addUser(array1));
     }
 }
